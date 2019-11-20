746c6ec40338b2350addb1f67bc36e43d6755547
flint <olusegun.sola19@gmail.com>
2019-11-20 23:46:54 +0100

    form

---
+++ src/YoutubePlayer.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import {Form, Container} from 'semantic-ui-react';
+
+class YoutubePlayer extends Component {
+    render() {
+        return(
+            <div>
+            <Container textAlign='center'>
+                <Form size='small'>
+                    <Form.Group>
+                        <Form.Input placeholder='search video' width={6} />
+                        <Form.Button content ='Search Videos'/>
+                    </Form.Group>
+                </Form>
+                </Container>
+        </div>
+        )
+       
+    }
+}
+
+
+export default YoutubePlayer;
